170be3a1e1e0a1fbcf2305acec093412e650f78c
YiweiNiu <xiaohuwangwang@qq.com>
2019-05-17 10:48:50 +0800

    use numpy to count

--- script/globalP.py
+++ script/globalP.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from os import path
+import pickle
+
+os.environ["OMP_NUM_THREADS"] = "1" # export OMP_NUM_THREADS=1
+os.environ["OPENBLAS_NUM_THREADS"] = "1" # export OPENBLAS_NUM_THREADS=1
+os.environ["MKL_NUM_THREADS"] = "1" # export MKL_NUM_THREADS=1
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1" # export VECLIB_MAXIMUM_THREADS=1
+os.environ["NUMEXPR_NUM_THREADS"] = "1" # export NUMEXPR_NUM_THREADS=1
+
+import numpy as np
 
 BINDIR = path.abspath(path.join(path.split(path.realpath(sys.argv[0]))[0], '..'))
 sys.path.append(BINDIR)
-
-import pickle
 from Ennet import permutation
 from Ennet import ennet
 
@@ -14,20 +22,17 @@ G = pickle.load(open(sys.argv[1], 'rb'))
 
 H = G.copy()
 
-tmp_dict = dict()
-i = 0
+tmp_list = list()
 for gene in H:
-    for score in H.nodes[gene]['emp_p_n']:
-        tmp_dict[i] = score
-        i += 1
+    tmp_list.extend(H.nodes[gene]['emp_p_n'])
 
-total_scores = i+1
+tmp_list = np.array(tmp_list)
+total_scores = len(tmp_list)
 
 for gene in H:
-    H.nodes[gene]['emp_p'] = sum(tmp_dict[i]>H.nodes[gene]['p_n'] for i in tmp_dict)/float(total_scores)
+    H.nodes[gene]['emp_p'] = np.sum(tmp_list>H.nodes[gene]['p_n'])/total_scores
 
 H = permutation.multi_test_pvalues(H)
 
 ennet.report(H, sys.argv[2])
-
 
